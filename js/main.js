8ba86005e997184908d6a79fe097c2c59b59d4a9
Alejandro Barria <alesergio972@gmail.com>
2023-05-22 19:46:10 -0300

    Clase 8

--- js/main.js
+++ js/main.js
@@ -1,34 +1,92 @@
-const $form = document.querySelector('#carta-a-santa');
-
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form['descripcion-regalo'].value;
-
-function validarNombre(nombre){
-    if(nombre.length === 0){
-        return 'Este campo debe tener al menos 1 caracter';
-    }else if (nombre.length >= 50){
-        return 'Este campo debe tener menos de 50 caracteres'
-    } else {
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return 'Este campo debe tener al menos 1 caracter';
+  } else if (nombre.length >= 50) {
+    return 'Este campo debe tener menos de 50 caracteres';
+  } else if (!/^[a-z]+$/i.test(nombre)) {
+    return 'El campo nombre solo acepta letras';
+  } else {
     return '';
-    }
+  }
 }
 
 function validarCiudad(ciudad) {
-    if(ciudad === ''){
-        return 'Debe seleccionar una ciudad'
-    }else {
+  if (ciudad === '') {
+    return 'Debe seleccionar una ciudad';
+  } else {
     return '';
-    }
+  }
 }
 
-function validarDescripcionRegalo(descripcionRegalo){
-    if(descripcionRegalo.length === 0) {
-        return 'Este campo debe tener al menos 1 caracter';
-    }else if(descripcionRegalo.length >= 100) {
-        return 'Este campo debe tener menos de 100 caracteres';
-    } else {
+function validarDescripcionRegalo(descripcionRegalo) {
+  if (descripcionRegalo.length === 0) {
+    return 'Este campo debe tener al menos 1 caracter';
+  } else if (descripcionRegalo.length >= 100) {
+    return 'Este campo debe tener menos de 100 caracteres';
+  } else if (!/^[A-z0-9]+$/.test(descripcionRegalo)) {
+    return 'El campo descripcion solo puede tener numeros y letras';
+  } else {
     return '';
-    }
+  }
 }
+
+function validarFormulario(event) {
+    const $form = document.querySelector("#carta-a-santa");
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+      nombre : errorNombre,
+      ciudad: errorCiudad,
+      'descripcion-regalo' : errorDescripcionRegalo
+    };
+
+
+    const esExito = manejarErrores(errores) === 0;
+    if(esExito) {
+      $form.className = 'oculto';
+      document.querySelector('#exito').className = '';
+    }
+    
+    event.preventDefault();
+}
+
+function manejarErrores(errores){
+
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+
+    let cantidadErrores = 0;
+
+    keys.forEach(function(key){
+      const error = errores[key];
+
+      if(error){
+        cantidadErrores ++;
+        $form[key].className = 'error';
+
+        const $error = document.createElement('li');
+        $error.innerText = error;
+        $errores.appendChild($error);
+      } else {
+        
+        $form[key].className = '';
+      }
+
+      
+    })
+
+    return cantidadErrores;
+
+}
+
+
+
+const $form = document.querySelector('#carta-a-santa');
+$form.onsubmit = validarFormulario;
--- js/pruebas.js
+++ js/pruebas.js
@@ -10,6 +10,17 @@ function probarValidarNombre() {
       'Este campo debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+
+  console.assert(
+    validarNombre('dfs223fff') === 'El campo nombre solo acepta letras',
+    'Validar nombre no valido que el nombre solo tenga letras',
+  );
+
+  // caso correcto
+  console.assert(
+    validarNombre('Alejandro') === '',
+    'Validar nombre falló con un nombre valido',
+  );
 }
 
 function probarValidarCiudad(){
@@ -17,19 +28,34 @@ function probarValidarCiudad(){
         validarCiudad('') === 'Debe seleccionar una ciudad',
         'Validar ciudad no validó que la ciudad no este vacia',
     );
+
+    console.assert(
+        validarCiudad('Buenos Aires') === '',
+        'Validar ciudad falló con un nombre de ciudad valido',
+    );
 }
 
 function probarValidarDescripcionRegalo(){
     console.assert(
         validarDescripcionRegalo('') === 'Este campo debe tener al menos 1 caracter',
         'Validar DescripcionRegalo no valido que la descripcion no esta vacia',
-    )
+    );
 
     console.assert(
         validarDescripcionRegalo('1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
         'Este campo debe tener menos de 100 caracteres', 
         'Validar DescripcionRegalo no valido que la descripcion no sea menor a 100 caracteres',
-    )
+    );
+
+    console.assert(
+        validarDescripcionRegalo('Regalo') === '',
+        'Validar descripcion regalo falló con una descripcion correcta',
+    );
+
+    console.assert(
+        validarDescripcionRegalo(',.,.,.,.,') === 'El campo descripcion solo puede tener numeros y letras',
+        'Validar descripcion regalo no valido que fuera solo numeros y letras',
+    );
 }
 
 probarValidarNombre();
